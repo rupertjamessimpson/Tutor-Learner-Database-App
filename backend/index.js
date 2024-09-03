f36d30db896f9e8c4898609e4bc5bf6048e968ac
rupertjamessimpson <87886963+rupertjamessimpson@users.noreply.github.com>
2024-09-03 00:33:45 -0400

    Created procfile and uninstalled cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 5002;
 
-app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
