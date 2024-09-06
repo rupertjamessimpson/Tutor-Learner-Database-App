31e2ce9d4b861c441858ae532a642082f6cfda22
rupertjamessimpson <87886963+rupertjamessimpson@users.noreply.github.com>
2024-09-06 13:41:39 -0400

    Edited index.js

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,12 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
