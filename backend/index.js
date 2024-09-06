7ec0e62faa75eb264ec428b6ad16425f81881c02
rupertjamessimpson <87886963+rupertjamessimpson@users.noreply.github.com>
2024-09-06 11:15:24 -0400

    Trying to deploy backend

--- backend/index.js
+++ backend/index.js
@@ -12,13 +12,3 @@ app.use('/api', routes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-const path = require('path');
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
-  });
-}
